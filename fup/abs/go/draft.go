cf2d8546a5bfd127e4c119596150ff3853d29a92
icgz25 <icarogustavo2507@gmail.com>
2026-03-23 17:38:17 +0000

    abs

---
+++ fup/abs/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+import "math"
+
+func main() {
+    var a float64
+    var b float64
+
+    fmt.Scan(&a, &b)
+
+    diferenca := math.Abs(a - b)
+
+    fmt.Println(diferenca)
+
+
+}
