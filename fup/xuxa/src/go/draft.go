e92b7ac15f262fc88a9385eddbb1ade4b86e4c7d
icgz25 <icarogustavo2507@gmail.com>
2026-05-04 18:09:19 +0000

    new

--- fup/xuxa/src/go/draft.go
+++ fup/xuxa/src/go/draft.go
@@ -1,5 +1,12 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var s string
+    fmt.Println(&s)
+
+    r := []rune(s)
+
+    for i := len(r) - 1; i >=0; i-- {
+        fmt.Print(string("o cheiro de pipoca ta rolando no ar"))
+    }
 }
